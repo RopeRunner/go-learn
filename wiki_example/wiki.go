0d42eee6eab28d929b2a3c3313f6c75afa2e8047
Vladislav Gorovoy <vldislav.operating@gmail.com>
2019-03-13 16:51:06 +0200

    [+] simple web app with 3 routes and serving/creating/modify static file

--- wiki_example/wiki.go
+++ wiki_example/wiki.go
@@ -1,14 +1,55 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 // Page is page structer type
 type Page struct {
 	Title string
 	Body  []byte
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
+// ViewHandler
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	page, err := loadPage(title)
+
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+
+	renderTemplate(w, "view", page)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+
+	p := &Page{Title: title, Body: []byte(body)}
+	p.Save()
+
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	page, err := loadPage(title)
+
+	if err != nil {
+		page = &Page{Title: title}
+	}
+
+	renderTemplate(w, "edit", page)
 }
 
 // Save is method to write daya to file system from Page
@@ -33,7 +74,8 @@ func main() {
 	p1 := &Page{Title: "SamplePage", Body: []byte("This is a simple page")}
 	p1.Save()
 
-	p2, _ := loadPage("SamplePage")
-
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
